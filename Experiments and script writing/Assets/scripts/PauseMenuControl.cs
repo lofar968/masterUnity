235541c5d0a65d9eedaa5bd27de46e366e5c7367
Delta1138 <38083409+Delta1138@users.noreply.github.com>
2018-04-12 10:17:58 -0700

    Added a continue button and arranged pause menu better
    Also foud a better way to send messages.
     [Script].SendMessage ("Message");
    in [script]
    Message()
    {
    //Insert code here
    }

--- Experiments and script writing/Assets/scripts/PauseMenuControl.cs
+++ Experiments and script writing/Assets/scripts/PauseMenuControl.cs
@@ -12,13 +12,27 @@ public class PauseMenuControl : MonoBehaviour {
     RawImage m_RawImage;
     public Texture blank_texture;
     public Texture paused_texture;
-    public GameObject[] pauseButton = new GameObject[3]; // if there wind up being more than 3 bts, CHANGE THIS #.
+    public GameObject[] pauseButton = new GameObject[3]; // if there wind up being more than 3 bts, CHANGE THIS #. Seems I may be able to in Unity.
     
     void Start()
     {
         m_RawImage = GetComponent<RawImage>(); //assigns mRI whatever RI object this is placed on
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        foreach (GameObject element in pauseButton)
+        {
+            element.SetActive(false);
+        }
+    }
+    public void Continue()
+    {
+        Time.timeScale = 1.0f;
+        m_RawImage.texture = blank_texture; //makes image invisible
+        gameIsPaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         foreach (GameObject element in pauseButton)
         {
             element.SetActive(false);
