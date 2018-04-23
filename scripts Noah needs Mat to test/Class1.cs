0b36f0009ecbf1f9d4cd2378b1d3ac52f44b1540
Delta1138 <38083409+Delta1138@users.noreply.github.com>
2018-04-23 13:52:55 -0700

    Started writing shotgunner AI script

--- scripts Noah needs Mat to test/Class1.cs
+++ scripts Noah needs Mat to test/Class1.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
 public class Class1
-{//copy from here--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-
+{
     void rotateTowardsPlayer()
     {
         //put your code here
     }
+    void BlockA()
+    {
+        //referece to flowchart
+    }
     
+    //copy from here--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-
+    private Rigidbody AIship = new Rigidbody();
+
+    void Start()
+    {
+        AIship = GetComponent<Rigidbody>();
+        BlockA();
+    }
 	void FixedUpdate()
 	{
 
