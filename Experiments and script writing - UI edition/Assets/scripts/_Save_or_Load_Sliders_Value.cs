002618a244c86f8afc684dcebf5b54d0eddff76c
Delta1138 <38083409+Delta1138@users.noreply.github.com>
2018-05-15 09:37:28 -0700

    Starting to get a save function

---
+++ Experiments and script writing - UI edition/Assets/scripts/_Save_or_Load_Sliders_Value.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Save_or_Load_Sliders_Value : MonoBehaviour {
+
+    public Slider ThisSlider;
+    public string SaveAs;
+
+	// Use this for initialization
+	void Start () {
+        ThisSlider.onValueChanged.AddListener(delegate { SaveNewValue(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
