2325b410eb66dcc0b586ee9e7b0e0992a1970c3f
Delta1138 <38083409+Delta1138@users.noreply.github.com>
2018-05-18 14:48:45 -0700

    Saving settings is now a thing
    By the way, saving things is EXTREMELY easy.

--- Experiments and script writing - UI edition/Assets/scripts/SendMessageToGM.cs
+++ Experiments and script writing - UI edition/Assets/scripts/SendMessageToGM.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class SendMessageToGM : MonoBehaviour {
     private GameObject GM;
+    public GameObject TestingGameObject;
 	// Use this for initialization
 	void Start () {
 		GM = GameObject.FindGameObjectWithTag("GM");
+        TestingGameObject.SendMessage("Message");
     }
 	void SendMessageToGM_LoadGame()
     {
         GM.SendMessage("PlayGame");
     }
+
+    //TESTING STUFF |----------------------------------------------------------------------------------------------------------------------------
+
+
 }
---
+++ Experiments and script writing - UI edition/Assets/scripts/zzzzzzzz_TestingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zzzzzzzz_TestingScript : MonoBehaviour {
+
+    public GameObject ThisObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+    void Message()
+    {
+        ThisObject.transform.position = new Vector3(0, 0, 0);
+        ThisObject.SetActive(true);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
