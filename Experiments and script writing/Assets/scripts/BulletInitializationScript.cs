6bd1182d960e93fb76732a0d714f466fbf338f83
Delta1138 <38083409+Delta1138@users.noreply.github.com>
2018-06-05 09:35:32 -0700

    We have a gun now

---
+++ Experiments and script writing/Assets/scripts/BulletInitializationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletInitializationScript : MonoBehaviour {
+    private Rigidbody rb;
+	
+    public void SetSpeed(float Speed)
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * Speed;
+    }
+    public void DestroyIn(int time)
+    {
+        Destroy(gameObject, time);
+    }
+
+}
